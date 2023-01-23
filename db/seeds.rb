c838b3ce2a78baafc4e4e6326e818ca65bee36f8
Kevin Leblond <leblond_kevin@hotmail.fr>
2023-01-24 00:30:19 +0100

    DB & Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+airlines = Airline.create([
+  {
+    name: "EasyJet",
+    image_url: "https://logos-download.com/wp-content/uploads/2016/03/EasyJet_emblem_logotype_logo_3-700x177.png"
+  },
+  {
+    name: "RyanAir",
+    image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Ryanair_logo_new.svg/2560px-Ryanair_logo_new.svg.png"
+  },
+  {
+    name: "Air France",
+    image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Air_France_Logo.svg/2560px-Air_France_Logo.svg.png"
+  },
+  {
+    name: "Qatar Airways",
+    image_url: "https://upload.wikimedia.org/wikipedia/fr/thumb/c/cb/Logo_Qatar_Airways.svg/1128px-Logo_Qatar_Airways.svg.png?20120112143212"
+  },
+  {
+    name: "Singapore Airlines",
+    image_url: "https://companieslogo.com/img/orig/C6L.SI_BIG-f66fd8ac.png?t=1651897371"
+  }
+])
+
+reviews = Review.create([
+  {
+    title: "Great",
+    description: "Great to fly in 1st",
+    score: 5,
+    airline: airlines.last
+  },
+   {
+    title: "Minimum effort",
+    description: "At least I arrived",
+    score: 1,
+    airline: airlines.first
+   }
+])
