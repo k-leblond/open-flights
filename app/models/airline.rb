596b5ae7ea861ec458efae2b67a6cd7d52cfe84c
Kevin Leblond <leblond_kevin@hotmail.fr>
2023-01-24 00:19:05 +0100

    init

---
+++ app/models/airline.rb
@@ -0,0 +1,16 @@
+class Airline < ApplicationRecord
+  has_many :reviews
+
+  before_create :slugify
+
+  #Transform name for better handling
+  def slugify
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+
+    reviews.average(:score).round(2).to_f
+  end
+
+end
