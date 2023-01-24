75ba3d9afcbad4f7d59557860aaedf5ff1a39f78
Kevin Leblond <leblond_kevin@hotmail.fr>
2023-01-24 01:24:52 +0100

    CRUD API

---
+++ app/controllers/api/v1/airlines_controller.rb
@@ -0,0 +1,57 @@
+module Api
+  module V1
+    class AirlinesController < ApplicationController
+
+      def index
+        airlines = Airline.all
+
+        render json: AirlineSerializer.new(airlines, options).serialized_json
+      end
+
+      def show
+        airline = Airline.find_by(slug: params[slug])
+
+        render json: AirlineSerializer.new(airline,options).serialized_json
+      end
+
+      def create
+        airline = Airline.new(airline_params)
+
+        if airline.save
+          render json: AirlineSerializer.new(airline).serialized_json
+        else
+          render json: { error: airline.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        airline = Airline.find_by(slug: params[slug])
+
+        if airline.save
+          render json: AirlineSerializer.new(airline, options).serialized_json
+        else
+          render json: { error: airline.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        airline = Airline.find_by(slug: params[slug])
+
+        if airline.destroy
+          head :no_content
+        else
+          render json: { error: airline.errors.messages }, status: 422
+        end
+      end
+
+      def airline_params
+        params.require(:airline).permit(:name, :image_url)
+      end
+
+      #Optional hash to pass reviews in the message
+      def options
+        @options ||= { include: %i[reviews]}
+      end
+    end
+  end
+end
